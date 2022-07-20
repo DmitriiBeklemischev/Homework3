30eca77dac1cc6861dff442713177e74411e2dc7
«Dmitrii <Ambal003@ya.ry>
2022-07-20 21:58:14 +0300

    Выполнил задание

---
+++ Task#3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+void getTablDeg(int num, int deg)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"{Math.Pow(i, deg)} ");
+    }
+}
+
+
+
+Console.WriteLine("Введите число N:");
+int num = int.Parse(Console.ReadLine());
+int deg = 3;
+getTablDeg(num, deg);
